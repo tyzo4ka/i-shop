18b0affdac32ba10dd71d46aebf848df5810ff2e
DinaraKA <d.karybekova@gmail.com>
2020-04-30 15:52:01 +0600

    Добавление модели Product и Category. Создание миграции.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,21 +1,30 @@
 # from django.contrib.auth.models import User
-# from django.db import models
+from django.db import models
 
 # PROJECT_STATUS = [('active', 'Active'), ('closed', 'Closed')]
 
 
-# class Status(models.Model):
-#     status_name = models.CharField(max_length=20, verbose_name='Status_name')
+class Category(models.Model):
+    category_name = models.CharField(max_length=20, verbose_name='Категория')
 
-#     def __str__(self):
-#         return self.status_name
+    def __str__(self):
+        return self.category_name
 
 
-# class Type(models.Model):
-#     type_name = models.CharField(max_length=20, verbose_name='Type_name')
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Товар')
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.PROTECT,
+                                verbose_name='Категория')
+    photo = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Изображение')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    in_stock = models.BooleanField(verbose_name='В наличии', default=True)
 
-#     def __str__(self):
-#         return self.type_name
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
 
 # class Project(models.Model):

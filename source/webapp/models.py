41ffe5065eb7b6eb13e621097f2160be87ea637c
tyzo4ka <tyzo4ka@gmail.com>
2020-05-11 16:42:15 +0600

    Добавлена модель адреса доставки

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
+CITY_CHOICES = (
+    ('Bishkek', 'Бишкек'),
+)
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, verbose_name='Категория')
@@ -31,3 +36,38 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
+
+
+class DeliveryAddress(models.Model):
+    city = models.CharField(max_length=20, choices=CITY_CHOICES, default=CITY_CHOICES[0][0], verbose_name="Город")
+    street = models.CharField(max_length=50, verbose_name="Улица")
+    building_number = models.CharField(max_length=10, verbose_name="Номер дома")
+    entrance_number = models.CharField(max_length=10, null=True, blank=True, verbose_name="Подъезд")
+    flat_number = models.CharField(max_length=5, null=True, blank=True, verbose_name="Номер квартиры/офиса")
+    additional_info = models.CharField(max_length=200, null=True, blank=True, verbose_name="Дополнительная информация")
+
+    class Meta:
+        verbose_name = 'Адрес доставки'
+        verbose_name_plural = 'Адреса доставки'
+
+
+# class Order(models.Model):
+#     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Пользователь', related_name='orders')
+#     first_name = models.CharField(max_length=100, verbose_name='Имя')
+#     last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+#     email = models.EmailField(max_length=50, verbose_name='Email')
+#     phone = models.CharField(max_length=20, verbose_name='Телефон')
+#
+#     products = models.ManyToManyField(Product, through='OrderProduct', through_fields=('order', 'product'),
+#                                       verbose_name='Товары', related_name='orders')
+#     status = models.CharField(max_length=20, choices=ORDER_STATUS_CHOICES, default=ORDER_STATUS_CHOICES[0][0],
+#                               verbose_name='Статус')
+#     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+#     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+#
+#     def __str__(self):
+#         return "{} / {}".format(self.email, self.phone)
+#
+#     class Meta:
+#         verbose_name = 'Заказ'
+#         verbose_name_plural = 'Заказы'

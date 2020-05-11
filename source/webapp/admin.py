02d5c2d5ee0726a2dbdf877c585a8a42ecf9de89
tyzo4ka <tyzo4ka@gmail.com>
2020-05-11 16:52:58 +0600

    добавлена регистрация модели адреса доставки в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from webapp.models import Category, Product, SubCategory
+from webapp.models import Category, Product, SubCategory, DeliveryAddress
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -20,7 +20,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_display_links = ['name', 'category', 'price', 'in_stock']
 
 
+class DeliveryAddressAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'city', 'street', 'building_number', 'entrance_number', 'flat_number', 'additional_info']
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(DeliveryAddress, DeliveryAddressAdmin)
 
